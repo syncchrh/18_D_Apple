7e4203764fb68c2d14f82bfc57a68429408bb396
이승우 주임 <sw.lee@hdel.co.kr>
2018-08-24 17:59:33 +0900

    // 데이터 정리

--- Module_Sseung/CSVFileIO.py
+++ Module_Sseung/CSVFileIO.py
@@ -5,7 +5,8 @@ import os
 
 
 def setCsv(filename, data):
-    with open(filename, 'a', encoding='UTF-8', newline="") as f:
-        csvWriter = csv.writer(f)
-        csvWriter.writerow(data)
+    f = open(filename, 'a', encoding='UTF-8', newline="")
+    csvWriter = csv.writer(f)
+    csvWriter.writerow(data)
+   # f.close()
 
